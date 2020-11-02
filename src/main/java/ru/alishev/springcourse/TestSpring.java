60b85b658444aa222309931a74da334da339c147
Heroglaber <alexander.budanin@gmail.com>
2020-11-02 06:51:31 +0300

    Implemented lessons 8 Bean Lifecycle (init-method, destroy-method, factory-method)

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -8,24 +8,27 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean1", Music.class);
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("musicBean2", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
-        boolean comparision = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println(comparision);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(25);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
-
-        firstMusicPlayer.playMusic();
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
+//        Music music = context.getBean("musicBean1", Music.class);
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparision = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparision);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(25);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+//
+//        firstMusicPlayer.playMusic();
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
 
         context.close();
     }
