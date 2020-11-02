6b4705bf2a3aedb2f4413b775ffbbbee5255d458
Heroglaber <alexander.budanin@gmail.com>
2020-11-02 08:41:23 +0300

    Lesson 12: @Scope, @Value WARN: @PostConstruct & @PreDestroy was moving to javax.annotation-api package

--- src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -2,18 +2,26 @@ package ru.alishev.springcourse;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Random;
 
 @Component
 public class ClassicalMusic implements Music {
 
+    @PostConstruct
+    private void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy");
+    }
+
     private String[] trackList = {"J.S. Bach: Suite No. 1 in G major",
             "Ludwig van Beethoven: Symphony No. 5 in C Minor",
             "Franz Schubert: 'Du bist die Ruh"};
-
-    private void init() {
-
-    }
 
     @Override
     public String getSong() {
