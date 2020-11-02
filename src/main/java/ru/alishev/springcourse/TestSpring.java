e8e1c4ddc20f6cb5fbf76e3618bd6dd57c5b02ed
Heroglaber <alexander.budanin@gmail.com>
2020-11-02 06:29:27 +0300

    Implemented lessons 7 bean scope

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -9,11 +9,23 @@ public class TestSpring {
         );
 
         Music music = context.getBean("musicBean1", Music.class);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean comparision = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparision);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(25);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        firstMusicPlayer.playMusic();
+        System.out.println(firstMusicPlayer.getName());
+        System.out.println(firstMusicPlayer.getVolume());
 
         context.close();
     }
